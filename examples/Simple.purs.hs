6585a12bb0597dfa46185a822d5d4b12600819a2
Phil Freeman <paf31@cantab.net>
2014-03-19 19:30:57 -0700

    Move over examples from compiler repo

---
+++ examples/Simple.purs.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Prelude
+import Data.Maybe
+
+main = do
+  let test1 = fromMaybe 0 $ do
+    a <- Just 1
+    b <- Just 2
+    return (a + b)
+  Debug.Trace.print test1
+~                                  
